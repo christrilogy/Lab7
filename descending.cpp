e62ff1e20a98263aa4d5cadac45a36e7de537752
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-03-03 14:21:48 -0500

    Create descending.cpp

---
+++ descending.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  for(int x=10; x>=1; x--)
+  {
+    for(int y=x; y>0; y--)
+      {
+   cout << "*";
+      }
+    cout << endl;
+  }
+    return 0;
+ }
